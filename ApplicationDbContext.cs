7c8934e493c34bf6926e78c002d6f865e6387310
jesus <jesuspenax11@gmail.com>
2022-09-25 11:55:21 -0400

    cambio

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Tarjetas
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+        {
+
+        }
+        public DbSet<Tarjeta> Tarjetas { get; set; }
+    }
+}
---
+++ Tarjeta.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tarjetas
+{
+    public class Tarjeta
+    {      
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Titular { get; set; }
+
+        [Required]
+        public string NumeroTarjeta { get; set; }
+
+        [Required]
+        public string FechaExpiracion { get; set; }
+
+        [Required]
+        public string CVV { get; set; }       
+    }
+}
